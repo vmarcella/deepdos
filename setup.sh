7e184e62a3b0e608e4a2a03aaea71ba09d89bee2
C3NZ <cenz@cenz.io>
2019-10-02 13:03:52 -0700

    [add] a setup script to run the file without having sudo permissions

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Create PCAP
+sudo groupadd pcap
+# add your user to the PCAP group
+sudo usermod -a -G pcap $USER
+# Change the ownership of tcpdump to be owned by PCAP
+sudo chgrp pcap /usr/sbin/tcpdump
+# give file capabilities for caputring raw packets and network admin items
+sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+# creaate a sym link for tcp dump in the users bin
+sudo ln -s /usr/sbin/tcpdump /usr/bin/tcpdump
