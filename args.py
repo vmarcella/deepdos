4d39ca2e0f16f35f6ebb2fabb0db2d7a246d55c7
C3NZ <cenz@cenz.io>
2019-10-04 00:25:00 -0700

    [add] more flags for deepdos and also make it possible to obtain interface data for use of our firewall

--- args.py
+++ args.py
@@ -1,3 +1,6 @@
+"""
+    The arg parsing 
+"""
 import argparse
 
 import psutil
@@ -22,6 +25,17 @@ def create_parser():
         default=None,
     )
 
+    # Read in the naughty count
+    parser.add_argument(
+        "-n",
+        action="store",
+        dest="naughty_count",
+        type=int,
+        help="the amount of malicious flows that can come from a given address",
+        default=10,
+    )
+
+    # Find all user interfaces
     parser.add_argument(
         "--find-interface",
         action="store_true",
@@ -30,7 +44,63 @@ def create_parser():
         default=False,
     )
 
+    # Activate firemode
+    parser.add_argument(
+        "--firewall-linux",
+        action="store_true",
+        dest="firewall_linux",
+        help="Turn on firewall mode for linux. The naughty list or -n argument is default to 10",
+        default=False,
+    )
+
+    parser.add_argument()
+
     return parser.parse_args()
+
+
+def obtain_interface_data(desired_interface):
+    """
+        Obtain the interface data and return a dictionary that contains
+        the information of each associated address to that interface
+    """
+    addrs = psutil.net_if_addrs()
+    data = {}
+
+    # Iterate through the all of the interfaces
+    if desired_interface in addrs:
+        nic = addrs[desired_interface]
+        for info in nic:
+            # add data for the current address family
+            data[f"{info.family}"] = {
+                "address": str(info.address),
+                "netmask": str(info.netmask),
+                "broadcast": str(info.broadcast),
+                "ptp": str(info.ptp),
+            }
+        return data
+    else:
+        # Couldn't find the requested interface
+        raise ValueError(f"Couldn't find the requested interface: {desired_interface}")
+
+
+def list_interface_data():
+    addrs = psutil.net_if_addrs()
+    for address, nic in addrs.items():
+        print(f"interface: {address}")
+        # Iterate through each associated address and it's information
+        for info in nic:
+            print("\tAssociated address:")
+            print(f"\t\tFamily: {info.family}")
+            print(f"\t\tAddress: {info.address}")
+            print(f"\t\tNetmask: {info.netmask}")
+            print(f"\t\tBroadcast: {info.broadcast}")
+            print(f"\t\tPTP: {info.ptp}")
+        print()
+
+    print(
+        "Pick one of these interfaces to listen to traffic on, and then rerun the command with it :)"
+    )
+    exit()
 
 
 def parse_args():
@@ -43,31 +113,25 @@ def parse_args():
 
     # Find all interfaces if specified and then exit
     if args.find_interface:
-        addrs = psutil.net_if_addrs()
-        for address, nic in addrs.items():
-            print(f"interface: {address}")
-            for info in nic:
-                print("\tAssociated address:")
-                print(f"\t\tFamily: {info.family}")
-                print(f"\t\tAddress: {info.address}")
-                print(f"\t\tNetmask: {info.netmask}")
-                print(f"\t\tBroadcast: {info.broadcast}")
-                print(f"\t\tPTP: {info.ptp}")
-            print()
+        # List all of the interface data
+        list_interface_data()
 
-        print(
-            "Pick one of these interfaces to listen to traffic on, and then rerun the command with it :)"
-        )
-        exit()
-
-    # Use the passed in interface or exit app
+    # Store the interface and interface data
     if args.interface:
         options["interface"] = args.interface
+        options["interface_data"] = obtain_interface_data(args.interface)
+        print(options["interface_data"])
         print(args.interface)
     else:
         print(
             "You need to provide a network interface for deepdos to listen on, or run --find-interface to list all of them."
         )
         exit()
+
+    # Check if firewall mode is enabled
+    if args.firewall_linux:
+        options["firewall_mode"] = True
+    else:
+        options["firewall_mode"] = False
 
     return options
