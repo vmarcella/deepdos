965641264ce3da4882af97ba068f8a0ee5eea177
C3NZ <cenz@cenz.io>
2019-10-03 18:31:57 -0700

    [add] the argument parser file

---
+++ args.py
@@ -0,0 +1,47 @@
+import argparse
+
+import psutil
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Welcome to deepdos, the machine learning/ai based ddos analysis/mitigation service"
+    )
+    # Read in the interface
+    parser.add_argument(
+        "-i",
+        action="store",
+        dest="interface",
+        help="the network interface for deepdos to listen to",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--find-interface", action="store_true", dest="find_interface", default=False
+    )
+
+    return parser.parse_args()
+
+
+def parse_args():
+    """
+        Parse the arguments of the arg parser
+    """
+    args = create_parser()
+    options = {}
+
+    # Find all interfaces
+    if args.find_interface:
+        addrs = psutil.net_if_addrs()
+        print(addrs.keys())
+        exit()
+
+    # Use the passed in interface or exit app
+    if args.interface:
+        options["interface"] = args.interface
+        print(args.interface)
+    else:
+        print(
+            "You need to provide a network interface for deepdos to listen on, or run --find-interface to list all of them."
+        )
+        exit()
