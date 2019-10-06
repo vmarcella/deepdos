7ffc50251034c9c1151916cac3e0d055b27d3686
C3NZ <cenz@cenz.io>
2019-10-06 12:13:18 -0700

    [add] setup and removal scripts for building this application from source yourself

---
+++ remove.sh
@@ -0,0 +1,22 @@
+OS="$(uname -s)"
+
+
+# Setup symbolic links for permissioning important network binaries
+if [ $OS = "Linux" ]; then
+    # Remove libpcap-dev, as it is not needed anymore
+    sudo apt remove libpcap-dev
+    
+    # Deactivate the virtual environment if it's running
+    if [[ "$VIRTUAL_ENV" != "" ]]; then
+        deactivate
+    fi
+
+    # Remove everything from the virtualenv
+    rm -r "$(pwd)/venv"
+    echo "It is now safe to delete the deepdos folder!"
+
+else
+    brew remove libpcap
+    echo "Non-linux based operating systems aren't fully supported yet, sorry :("
+fi
+
--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ OS="$(uname -s)"
 if [ "$OS" = "Linux" ]; then
     # Install dependencies
     sudo apt install libpcap-dev
-
-    # Install virtualenv to the current user if not already installed
-    pip3 install virtualenv --user
-
-    # Create the virtualenv and install dependencies locally
-    virtualenv -p python3 venv
-    source venv/bin/activate 
-    pip3 install -r requirements.txt
 else
+    # Macos setup coming soon
     brew install libpcap
     echo "Non-linux based operating systems aren't supported yet, sorry :("
 fi
 
+# Install virtualenv to the current user if not already installed
+pip3 install virtualenv --user
+
+# Create the virtualenv and install dependencies locally
+virtualenv -p python3 venv
+source "$(pwd)/venv/bin/activate"
+pip3 install -r requirements.txt
