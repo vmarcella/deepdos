9457b07b130835365c19e335c82e63f046f4622a
C3NZ <cenz@cenz.io>
2019-10-06 18:47:58 -0700

    [add] a conf file for global variables that need to be accessed by the program can be accessed

---
+++ src/conf.py
@@ -0,0 +1,10 @@
+"""
+    Global variables
+"""
+import os
+
+# Get the root directory for deepdos
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Latest stable model
+LATEST_STABLE_MODEL = "lr-stable-0.9.0.pickle"
