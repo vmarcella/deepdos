8f02c270c99fc34b8d7fe9035cf9657517819dbc
C3NZ <cenz@cenz.io>
2019-10-08 16:20:50 -0700

    [refactor] conf subprocesses to all be inside of the processes module

--- deepdos/conf.py
+++ deepdos/conf.py
@@ -1,10 +1,11 @@
 """
     Global variables
 """
-import getpass
 import os
-import subprocess
 import sys
+
+from deepdos.utils.processes import (proc_create_linux_symlink,
+                                     proc_find_deepdos)
 
 # Get the root directory for deepdos
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -19,8 +20,8 @@ LATEST_STABLE_MODEL = "lr-stable-0.9.0.pickle"
 
 def create_etc_dirs():
     """
-        Create etc logging directories for all subprocesses
-        to function properly
+        Create all directories needed for deepdos to properly execute. Only
+        runs if the logs directory isn't created.
     """
     # Load in the folders that don't exist
     if not os.path.exists(f"{ETC_DIR}/logs"):
@@ -36,66 +37,9 @@ def setup_root_access():
     """
     if not os.path.exists(f"{ETC_DIR}/.haslink") and os.geteuid() != 0:
 
-        # Obtain the deepdos bin location post install
-        get_deepdos = ["which", "deepdos"]
-        process = subprocess.Popen(get_deepdos, stdout=subprocess.PIPE)
-        location = process.stdout.readline()
-        process.stdout.close()
-        exit_status = process.wait()
-
-        if exit_status or not location:
-            print("You didn't properly install deepdos :/")
-            exit(1)
-
         if sys.platform == "linux":
-            user_input = input(
-                "deepdos can setup a symlink inside of /usr/bin/deepdos for you if you'd\n"
-                "like to be able to easily access deepdos via sudo. If you can already access\n"
-                "deepdos with sudo, then you can just say exit or press N.\n"
-                "Would you like deepdos to try and create a symlink? [y/N]"
-            )
-
-            if user_input != "y":
-                print("Shutting down. Cannot access network utilities without sudo.")
-                exit()
-
-            # Create link command
-            create_link = [
-                "sudo",
-                "ln",
-                "-s",
-                location.decode("utf-8").rstrip(),
-                "/usr/bin/deepdos",
-            ]
-
-            # Spawn the create_link subprocess
-            process = subprocess.Popen(
-                create_link,
-                stdout=subprocess.PIPE,
-                stdin=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-
-            print(
-                "For initial setup, deepdos needs your password to create a symlink @ /usr/bin/deepdos\n"
-                "to ensure that your sudo user has it! You can enter your password down below if prompted."
-            )
-
-            # Get the first
-            _, std_err = process.communicate(input="")
-
-            # Obtain the sudo password from the user
-            while std_err.decode("utf-8") == f"[sudo] password for {getpass.getuser()}":
-                sudo_password = str(getpass.getpass(prompt=std_err.decode("utf-8")))
-                _, std_err = process.communicate(input=sudo_password)
-
-            exit_status = process.wait()
-
-            # Couldn't authenticate successfully
-            if exit_status:
-                print(std_err.decode("utf-8").rstrip())
-                exit(1)
-
+            location = proc_find_deepdos()
+            proc_create_linux_symlink(location)
             open(f"{ETC_DIR}/.haslink", "w+").close()
             print("You can now rerun deepdos as root user!")
             exit()
--- deepdos/utils/processes.py
+++ deepdos/utils/processes.py
@@ -2,9 +2,8 @@
     Module for spawning subprocesses within deepdos to carry out operations 
     that this application isn't capable of doing.
 """
+import getpass
 import subprocess
-
-from deepdos.conf import ETC_DIR, ROOT_DIR
 
 
 def proc_capture_pcap(interface, line_count=1000):
@@ -45,18 +44,21 @@ def proc_capture_pcap(interface, line_count=1000):
     return output_list
 
 
-def proc_execute_cicflowmeter():
+def proc_execute_cicflowmeter(etc_dir: str):
     """
         Execute the cicflowmeter to create the flow csv from the pcap file.
+
+        Args:
+            etc_dir - The parent directory of all non-code files
     """
     # cic flowmeter command that retrieves all .pcap files from pcap_info and creates
     # a flow output for each .pcap file
-    cic_cmd = ["sh", "cfm", f"{ETC_DIR}/pcap_info", f"{ETC_DIR}/flow_output"]
+    cic_cmd = ["sh", "cfm", f"{etc_dir}/pcap_info", f"{etc_dir}/flow_output"]
 
     # Open up the cic flowmeter
     process = subprocess.Popen(
         cic_cmd,
-        cwd=f"{ETC_DIR}/external/CICFlowMeter-4.0/bin",
+        cwd=f"{etc_dir}/external/CICFlowMeter-4.0/bin",
         stdout=subprocess.DEVNULL,
     )
 
@@ -66,3 +68,75 @@ def proc_execute_cicflowmeter():
     # If the exit wasn't graceful, throw an error
     if exit_status:
         raise subprocess.CalledProcessError(exit_status, cic_cmd)
+
+
+def proc_find_deepdos():
+    """
+        Find where deepdos is located in your path
+    """
+    # Obtain the deepdos bin location post install
+    get_deepdos = ["which", "deepdos"]
+
+    # Spawn new process
+    process = subprocess.Popen(get_deepdos, stdout=subprocess.PIPE)
+    location = process.stdout.readline()
+    process.stdout.close()
+    exit_status = process.wait()
+
+    if exit_status or not location:
+        print(
+            "deepdos couldn't be found on your path. Please try adding deepdos to your path"
+            "or executing it as sudo."
+        )
+        exit(1)
+    return location.decode("utf-8").rstrip()
+
+
+def proc_create_linux_symlink(src_location: str):
+    """
+        Create a symlink for deepdos so that it can be found on the sudo secure path.
+
+        Args:
+            src_location: the location where deepdos is installed as a string
+    """
+    user_input = input(
+        "deepdos can setup a symlink inside of /usr/bin/deepdos for you if you'd\n"
+        "like to be able to easily access deepdos via sudo. If you can already access\n"
+        "deepdos with sudo, then you can just say exit or press N.\n"
+        "Would you like deepdos to try and create a symlink? [y/N]"
+    )
+
+    if user_input != "y":
+        print("Shutting down. Cannot access network utilities without sudo.")
+        exit()
+
+    # Create link command
+    create_link = ["sudo", "ln", "-s", src_location, "/usr/bin/deepdos"]
+
+    # Spawn the create_link subprocess
+    process = subprocess.Popen(
+        create_link,
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    print(
+        "For initial setup, deepdos needs your password to create a symlink @ /usr/bin/deepdos\n"
+        "to ensure that your sudo user has it! You can enter your password down below if prompted."
+    )
+
+    # Get cmd output
+    _, std_err = process.communicate()
+
+    # Obtain the sudo password from the user
+    while std_err.decode("utf-8") == f"[sudo] password for {getpass.getuser()}":
+        sudo_password = str(getpass.getpass(prompt=std_err.decode("utf-8")))
+        _, std_err = process.communicate(input=sudo_password)
+
+    exit_status = process.wait()
+
+    # Couldn't authenticate successfully, display error
+    if exit_status:
+        print(std_err.decode("utf-8").rstrip())
+        exit(1)
