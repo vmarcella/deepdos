009bc65a65b04ef5318db648b9c2861026616fee
C3NZ <cenz@cenz.io>
2019-10-18 00:10:33 -0700

    [add] test structure

---
+++ deepdos/tests/test_network.py
@@ -0,0 +1,18 @@
+"""
+    Network usage model
+"""
+import unittest
+
+import deepdos.utils.network as network
+
+
+class NetworkTests(unittest.TestCase):
+    def test_log_ip(self):
+        print("yeet")
+
+    def test_next_thing(self):
+        print("hello")
+
+
+if __name__ == "__main__":
+    unittest.main()
