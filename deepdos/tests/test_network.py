e45dcd17ff54bc0d50f0983b3c42e71851c1c371
C3NZ <cenz@cenz.io>
2019-10-18 01:54:30 -0700

    [add] more tests that include mocking, which will be the future for testing

--- deepdos/tests/test_network.py
+++ deepdos/tests/test_network.py
@@ -10,9 +10,6 @@ class NetworkTests(unittest.TestCase):
     def test_log_ip(self):
         print("yeet")
 
-    def test_next_thing(self):
-        print("hello")
-
 
 if __name__ == "__main__":
     unittest.main()
---
+++ deepdos/tests/test_processes.py
@@ -0,0 +1,59 @@
+import subprocess
+import unittest
+
+import deepdos.utils.processes as proc
+
+import mock
+
+
+def proc_close_success():
+    """
+        Successfully closed command
+    """
+    return 0
+
+
+def proc_close_fail():
+    """
+        Failed to close command
+    """
+    return 1
+
+
+def stub_pcap_line_data():
+    """
+        Stub the pcap line data inside of the test
+    """
+    for value in ["hello", "432432"]:
+        yield value
+
+
+class ProcessTests(unittest.TestCase):
+    @mock.patch("deepdos.utils.processes.subprocess.Popen")
+    def test_successful_capture_pcap(self, mock_process):
+        """
+            Test to ensure that pcap succeeds under normal capture
+        """
+        # Initialize stub functions
+        mock_process.return_value.wait = proc_close_success
+        mock_process.return_value.stdout.readline = stub_pcap_line_data
+        mock_process.return_value.stdout.close.return_value = 0
+        output_list = proc.proc_capture_pcap("enp3s0", 2)
+
+        # Assert we get the length of the desired data
+        assert len(output_list) == 2
+
+    @mock.patch("deepdos.utils.processes.subprocess.Popen")
+    def test_failed_pcap(self, mock_process):
+        """
+            Test to ensure that pcap fails when process closes
+        """
+        # Initialize stub functions
+        mock_process.return_value.wait = proc_close_fail
+        mock_process.return_value.stdout.readline.return_value = stub_pcap_line_data
+        mock_process.return_value.stdout.close.return_value = 0
+
+        # Ensure that the subprocess throws an error due to a failed process close
+        self.assertRaises(
+            subprocess.CalledProcessError, proc.proc_capture_pcap, "enp3s0", 1
+        )
